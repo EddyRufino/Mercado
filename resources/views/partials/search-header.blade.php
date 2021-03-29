8558fd7083aaec78a10d54eaca4bff0c625c4215
Eddy Rufino <eddyjaair@gmail.com>
2021-03-29 10:50:21 -0500

    Refactoring [buttons create, icons in folders and resolve list deuda]

--- resources/views/partials/search-header.blade.php
+++ resources/views/partials/search-header.blade.php
@@ -1,5 +1,14 @@
-  <div class="d-flex justify-content-around mt-3">
-      <h4 class="text-secondary font-weight-bold">{{ $title }} <a class="btn btn-primary btn-sm" href="{{ route($linkCreate) }}">{{ $titleCreate }}</a>
+  <div class="row d-flex justify-content-around mt-3">
+      <h4 class="text-secondary font-weight-bold">{{ $title }}
+        <a href="{{ route($linkCreate) }}" data-toggle="tooltip" data-placement="top" title="Agregar">
+          <i class="fas fa-plus-square"></i>
+        </a>
+        <a href="{{ route($linkPDF) }}" class="text-danger" data-toggle="tooltip" data-placement="top" title="Exportar PDF">
+          <i class="fas fa-file-pdf"></i>
+        </a>
+        <a href="{{ route($linkEXCEL) }}" class="text-success" data-toggle="tooltip" data-placement="top" title="Exportar EXCEL">
+          <i class="fas fa-file-excel"></i>
+        </a>
       </h4>
       <div class="">
           <form method="GET" action="{{ route($linkAction) }}" class="form-inline">
--- resources/views/puestos/deudas/index.blade.php
+++ resources/views/puestos/deudas/index.blade.php
@@ -5,36 +5,38 @@
         @if (auth()->user()->hasRoles(['admin']))
             <div class="row justify-content-center">
                 <div class="col-md-6">
-                    <div class="list-group mt-3">
+                    <li class="list-group mt-3">
                       <button type="button" class="list-group-item list-group-item-action bg-secondary d-flex justify-content-between">
                         <span>Listado de Deudas</span>
 
-                        @if ($deudas->count() >= 30 )
+                        {{-- @if ($deudas->count() >= 30 )
                             <span>Días sin pagar: <strong  class="text-danger">{{ $deudas->count() }}</strong>
                             </span>
                         @else
                             <span>Días sin pagar: <strong>{{ $deudas->count() }}</strong>
                             </span>
-                        @endif
+                        @endif --}}
                       </button>
 
-                      @forelse ($deudas as $deuda)
-                          <div class="list-group-item list-group-item-action d-flex justify-content-between">
+                    @forelse ($deudas as $deuda)
+                        <div class="list-group-item list-group-item-action">
                             <div class="row">
                                 <div class="col-md-12">
-                                    <span class="text-secondary" style="font-size: 1.1rem">
+                                    <span class="text-secondary font-weight-bold" style="font-size: 1.1rem">
                                         {{ $deuda->puesto->user->name }} {{ $deuda->puesto->user->apellido }}
                                     </span>
                                 </div>
-                                <div class="col-sm-12 col-md-6 d-flex justify-content-between">
+                                <div class="col-sm-12 col-md-7 d-flex justify-content-between">
                                     <span class="text-secondary">
+                                        @include('icons.icon-home')
                                         Puesto {{ $deuda->puesto->num_puesto }}
                                     </span>
                                     <span class="text-secondary">
+                                        @include('icons.icon-date')
                                         {{ $deuda->fecha }}
                                     </span>
                                 </div>
-                                <div class="col-sm-12 col-md-6 d-flex justify-content-around">
+                                <div class="col-sm-12 col-md-5 d-flex justify-content-around">
                                     <span class="text-secondary">S/. {{ $deuda->monto_sisa }}</span>
 
                                   @auth
@@ -45,10 +47,9 @@
                                             {{ csrf_field() }} {{ method_field('DELETE') }}
                                       <button class="btn btn-xs btn-link p-0 m-0"
                                         onclick="return confirm('¿Estás seguro del pago?')" data-toggle="tooltip" data-placement="top" title="Pagar Deuda">
-                                        <svg width="1.4em" height="1.4em" viewBox="0 0 16 16" class="text-warning " fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z"/>
-                                            <path d="M2 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z"/>
-                                        </svg>
+
+                                        @include('icons.icon-pay')
+
                                       </button>
                                     </form>
 
@@ -56,13 +57,13 @@
                                   @endauth
                                 </div>
                             </div>
-                          </div>
-                    </div>
+                        </div>
+                    </li>
                     @empty
                         <li class="list-group-item border-0 mb-3 shadow-sm">No hay deudas para mostrar</li>
                     @endforelse
 
-                    <div class="overflow-auto mt-2">
+                    <div class="overflow-auto mt-1">
                         {{ $deudas->links() }}
                     </div>
                 </div>
